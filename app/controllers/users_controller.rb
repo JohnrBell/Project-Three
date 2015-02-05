f57c93469c9351c278d8098f8d5213ab9afb139f
japaranian <mina.talai@gmail.com>
2015-02-05 17:31:15 -0500

    completed user controller, views, added session controller for login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
-class Users < ApplicationController
+class UsersController < ApplicationController
 
-	def new
+	def index #user login page, also has a link to sign up as a new user
+		
+	end
+
+
+	def new #new user sign up page, renders new.html.erb
 
 	end
 
-	def create
-		
+
+	def create #creates new user when user hits submit button in the sign up page
+		new_user = User.new({name: params["name"], email: params["email"], password: params[:password]})
+		new_user.save
+		redirect_to '/articles'
 	end
 
 end
