0bf6d9023a4366ab9e2250a543fbf87e79ea0974
JohnrBell <johnrbell@gmail.com>
2015-02-05 15:32:10 -0500

    Basic Pending Articles functionality half done

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,19 @@ root 'users#index'
 get '/users/new' => 'users#new'                                     #gets 'create new user' view
 post '/users/create' => 'users#create'                              #post to create a new user
 
-post '/session/create' => 'sessions#create'                         #post to start a session at login 
-post '/session/destroy' => 'sessions#destroy'                       #post to end a session at logout
+post '/sessions/create' => 'sessions#create'                         #post to start a session at login 
+post '/sessions/destroy' => 'sessions#destroy'                       #post to end a session at logout
 
 get '/articles' => 'articles#index'                                 #landing page (shows all articles)
 post '/articles/create' => 'articles#create'                        #post a pending article to articles
 get '/articles/:id' => 'articles#view'                              #shows a specific article and its comments
 
 get '/pending' => 'p_articles#index'                                #landing admin page (shows all pending articles)
-post '/pending/create' => 'p_articles#create'                       #post reddit api results to pending
-post '/pending/destory' => 'p_articles#destroy'                     #post that accepts a pending
+post '/pending/create' => 'p_articles#create'                       #post that accepts a pending
+post '/pending/destroy' => 'p_articles#destroy'                     #post that accepts a pending
 
-post '/article/:id/comment' => 'comment#create'                     #post to post a comment to article :id
-post '/article/:id/comments/:id/subcomment' => 'subcomment#create'  #post to post a subcomment to a comment
+post '/articles/:id/comments' => 'comments#create'                     #post to post a comment to article :id
+post '/articles/:id/comments/:id/subcomments' => 'subcomments#create'  #post to post a subcomment to a comment
 
 
   # The priority is based upon order of creation: first created -> highest priority.
