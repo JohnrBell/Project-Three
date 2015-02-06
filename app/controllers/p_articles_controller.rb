99a9140cfb7c2164a733f7dc99c595f30e3c987d
Umer Dar <umerbashirdar@gmail.com>
2015-02-06 01:21:34 -0500

    displayed images on page

--- app/controllers/p_articles_controller.rb
+++ app/controllers/p_articles_controller.rb
@@ -7,13 +7,13 @@ class PArticlesController < ApplicationController
 
 	def create
 		P_Article.create({title: params[:title], img_url: params[:img_url]})
-		render :nothing => true 
+		render :nothing => true
 	end
 
 	def	destroy
 		idtokill = params[:id].to_i
 		P_Article.destroy(idtokill)
-		render :nothing => true 
+		render :nothing => true
 	end
 
 end
